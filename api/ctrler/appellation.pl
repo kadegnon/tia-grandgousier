73987af595875cb70c804fb07cc03406f60390f1
kadegnon <kodjo.adegnon@student.unamur.be>
2018-03-01 13:08:15 +0100

    Ajout de predicat pour les appelations de vin

---
+++ api/ctrler/appellation.pl
@@ -0,0 +1,16 @@
+% Export appellation predicats
+:- module(appellation).
+
+
+
+% Base de Connaissance
+
+appellation(bordeaux).
+appellation('Vallée du rhone et de Languedoc').
+appellation(beaujolais).
+appellation(bourgogne).
+appellation('Val de Loire').
+appellation('vins Blanc').
+appellation('Champagne').
+appellation(cognac).
+
