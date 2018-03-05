3b32f6c40af429e261542a182ec7a0c3f58452e1
kadegnon <kodjo.adegnon@student.unamur.be>
2018-03-05 18:53:02 +0100

    Ajout de components et pages

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,27 +2,36 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <Chat/>
+    <Vins/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import HelloWorld from './pages/HelloWorld';
+  import Chat from './pages/Chat';
+  import Vins from './pages/Vins';
+  import About from './pages/About';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      Chat,
+      Vins,
+      About,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ web/src/pages/Vins.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chat">
+    <h1></h1>
+    <h2>Essential Links</h2>
+    <ul v-model:"m in messages"> 
+      <chat-msg :msg="m.content" :type="m.type"> </chat-msg>
+    </ul>
+    <user-choice>
+
+    </user-choice>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .chat {
+    max-width: 640px;
+    margin: 0 auto
+  }
+
+  .chat ul {
+    padding: 20px 50px
+  }
+</style>
