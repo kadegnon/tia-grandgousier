10404aa6bbfa5153f3c84ceeb8b4a9754750c4a3
kadegnon <kodjo.adegnon@student.unamur.be>
2018-03-05 18:32:01 +0100

    Config du web app avec vue-cli

--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
+/* eslint-disable*/
+import Vue from 'vue';
 
-import boot from './boot'
-import App from './App.vue'
+// import boot from './boot';
+import App from './App';
 
 
 
-// Instanciate the app and mount it to the DOM (#app)
-new Vue({ // eslint-disable-line no-new
-  el: '#root',
-  ...boot,
-  render: (h) => h(App)
-})
+Vue.config.productionTip = false;
+
+new Vue({
+  el: '#app',
+  // ...boot,
+  render: h => h(App),
+});
