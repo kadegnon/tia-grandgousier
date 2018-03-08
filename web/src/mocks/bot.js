56c7bebb8bcc1bd08d02e1698005733eb851dd50
kadegnon <kodjo.adegnon@student.unamur.be>
2018-03-08 16:14:33 +0100

    Ajout de mock pour API request vers boot/

---
+++ web/src/mocks/bot.js
@@ -0,0 +1,17 @@
+export default {
+  "routes" : [
+  {
+    "method": "GET",
+    "url": "bot/",
+    "response": [
+      "bordeaux",
+      "Vallée du rhone et de Languedoc",
+      "beaujolais",
+      "bourgogne",
+      "Val de Loire",
+      "vins Blanc",
+      "Champagne",
+      "cognac"
+      ]
+  }
+]};
