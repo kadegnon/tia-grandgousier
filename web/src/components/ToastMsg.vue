2484557b6dbf3f71e0313982d35a004a8923bbc3
kadegnon <kodjo.adegnon@student.unamur.be>
2018-03-10 18:27:55 +0100

    Ajout emit global pour les ToastMsg

--- web/src/components/ToastMsg.vue
+++ web/src/components/ToastMsg.vue
@@ -1,89 +1,103 @@
 <template>
   <div id="toast-msg" v-bind:class="[type]" v-show="visible">
-    <span class="btn-close" @click="">&times;</span>
+    <span class="btn-close" @click="close">&times;</span>
     <span>{{msg}}</span>
   </div>
 
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   name: "ToastMsg",
   data() {
     return {
-      msg : 'Yello, mon p*t**n de message ! ',
-      type : 'info',
-      visible : false,
+      msg: "Yello, mon p*t**n de message ! ",
+      type: "info",
+      visible: false,
+      autoCloseTimeoutID: 0
     };
   },
-  mounted (){
-    this.$on('msg-error',msg => {
-        this.msg = msg;
-        this.type = 'error';
-        this.initAutoClose();
-    });
-
-    this.$on('msg-info',msg => {
-        this.msg = msg;
-        this.type = 'info'
-        this.initAutoClose();
-    });
-
-    this.$on('msg-success',msg => {
-        this.msg = msg;
-        this.type = 'success'
-      this.initAutoClose();
-    });
+  watch: {
+    type: function() {
+      this.visible = true;
+    }
   },
-  methods : {
-    initAutoClose(time=4){
-        setTimeout(() => {
-          console.log('Autclosing in ',time);
-          this.close();
-        },time*1000);
+  created() {
+    this.$bus.$on('msg-info', msg => this.display(msg));
+    this.$bus.$on('msg-error', msg => this.display(msg,'error'));
+    this.$bus.$on('msg-success', msg => this.display(msg,'success'));
+  },
+  beforeDestroy() {
+    this.$bus.$off(['msg-info', 'msg-error','msg-success']);
+    console.log('Destroy comp. ',this.autoCloseTimeoutID);
+    clearInterval(this.autoCloseTimeoutID);
+  },
+  methods: {
+    initAutoClose(time = 4) {
+      this.autoCloseTimeoutID = setTimeout(() => {
+        console.log("Toast Autclosed in ", time);
+        this.close();
+      }, time * 1000);
     },
 
-    close(){
+    close() {
       this.visible = false;
-    }
+      clearInterval(this.autoCloseTimeoutID);
+    },
 
+    display(msg, type='info') {
+      this.msg = msg;
+      this.type = type;
+      this.initAutoClose();
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #toast-msg {
-    min-width: 250px;
-    margin-left: -125px;
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    border-radius: 2px;
-    padding: 16px;
-    position: fixed;
-    z-index: 1;
-    bottom: 30px;
-    left: 50%;
-  }
+#toast-msg {
+  min-width: 250px;
+  margin-left: -125px;
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  border-radius: 2px;
+  padding: 16px;
+  position: fixed;
+  z-index: 1;
+  bottom: 30px;
+  left: 50%;
+}
 
-  #toast-msg.success {background-color: aquamarine; color:black}
-  #toast-msg.info {background-color: #2196F3;}
-  #toast-msg.warning {background-color: #ff9800;}
-  #toast-msg.error {background-color: crimson;}
+#toast-msg.success {
+  background-color: aquamarine;
+  color: black;
+}
+#toast-msg.info {
+  background-color: #2196f3;
+}
+#toast-msg.warning {
+  background-color: #ff9800;
+}
+#toast-msg.error {
+  background-color: crimson;
+}
 
-  #toast-msg > .btn-close {
-    margin-left: 15px;
-    color: white;
-    font-weight: bold;
-    float: right;
-    font-size: 22px;
-    line-height: 20px;
-    cursor: pointer;
-    transition: 0.3s;
-  }
+#toast-msg > .btn-close {
+  margin-left: 15px;
+  color: white;
+  font-weight: bold;
+  float: right;
+  font-size: 22px;
+  line-height: 20px;
+  cursor: pointer;
+  transition: 0.3s;
+}
 
-  .btn-close:hover {
-    color: black;
-  }
+.btn-close:hover {
+  color: black;
+}
 </style>
