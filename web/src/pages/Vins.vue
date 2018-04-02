ee1d187dcc3aca58eeb2be95acc99e705f67c5dd
kadegnon <kodjo.adegnon@student.unamur.be>
2018-04-02 17:49:57 +0200

    Suppression de commentaires dans getListVins

--- web/src/pages/Vins.vue
+++ web/src/pages/Vins.vue
@@ -95,11 +95,11 @@ export default {
     },
 
     getListVins () {
-      /*this.$http.get(API_URL_TO_VINO)
+      this.$http.get(API_URL_TO_VINO)
             .then(res => this.list = res.data)
-            .catch(e => this.$bus.$emit('msg-error','Impossible de recuperer le catalogue'))
+            .catch(e => this.$bus.$emit('msg-error','Impossible de recuperer le catalogue des vins'))
             // .catch(err => this.$emit('msg-error',err.body.statusText));
-    */
+
     }
   }
 };
