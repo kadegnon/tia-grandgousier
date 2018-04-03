ff2a7150bad275dffd9179d6eabacfc1e5a2015e
kadegnon <kodjo.adegnon@student.unamur.be>
2018-04-03 18:26:19 +0200

    Ajout de gestion de Cache pour les list

--- web/src/services/api-vino.js
+++ web/src/services/api-vino.js
@@ -11,20 +11,45 @@ import Vue from 'vue';
 const API_URL_TO_VINO = 'vino/';
 const Api = Vue.http;
 const ApiResoucreToVino = Vue.resource('vino{/id}');
+const Storage = localStorage;
 
+const isInStorage = key => Storage.hasOwnProperty(key);
+
+const getFromStorage = key => JSON.parse(Storage.getItem(key));
+
+const addToStorage = (key, item) => Storage.setItem(key, JSON.stringify(item));
+
+/**
+ * Retrieve the list either from the CacheStorage or directly through the API.
+ * @param {String} url API url where to get the list.
+ * @returns {Promise} containing the resolved list.
+ */
+const getList = url => {
+  const key = 'API_LIST_FOR_'+url.replace(/\//g, '').toUpperCase();
+  if(isInStorage(key)){
+    return Promise.resolve(getFromStorage(key));
+  }else{
+    return Api.get(url)
+        .then(({data}) => {
+          addToStorage(key, data);
+          return data;
+        })
+  }
+};
 
 // -------------------------------------------------------------------
 // Exports
 
+
 export const APIVino = {
 
-  getListAppellations : _ => Api.get("appellations/").then(res => res.data),
+  getListAppellations : _ =>  getList("appellations/"),
 
-  getListCouleurs : _ => Api.get("couleurs/").then(res => res.data),
+  getListCouleurs : _ => getList("couleurs/") ,
 
-  getListPlats : _ => Api.get("plats/").then(res => res.data),
+  getListPlats : _ => getList("plats/") ,
 
-  getListServices : _ => Api.get("services/").then(res => res.data),
+  getListServices : _ => getList("services/") ,
 
   getListVins : _ => Api.get("vino/").then(res => res.data),
 
