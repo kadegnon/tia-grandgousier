c3a15672ed29d1908fe2af0b351fa9b3bacad4db
kadegnon <kodjo.adegnon@student.unamur.be>
2018-04-09 19:20:06 +0200

    Ajout env var pour connexion API en prod

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL : '"http://ec2-54-70-205-23.us-west-2.compute.amazonaws.com:8081/api"'
+  // ,API_URL : '"http://54.70.205.23:8081/api"'
 }
