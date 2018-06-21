385f1991748e4c801bfdeaeecbe4d22c3e5e1717
kadegnon <kodjo.adegnon@student.unamur.be>
2018-06-22 00:09:22 +0200

    Config && build

--- web/config/dev.env.js
+++ web/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL : '"http://192.168.1.42:8081/api"'
+  API_URL : '"http://192.168.1.32:8081/"'
 });
